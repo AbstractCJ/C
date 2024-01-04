71d97c6f1ba3fa75116e327ded218a50012708be
User <User@DESKTOP-6JPNDEN.lan>
2024-01-04 10:53:02 -0500

    Learning

---
+++ src/C/C.java
@@ -0,0 +1,11 @@
+package C;
+
+public class C {
+	public static void main(String[] args) {
+		
+		System.out.println("Here is hoping");
+		System.out.println("B");
+		System.out.println("Getting excited!");
+	}
+
+}
