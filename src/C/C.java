dde0b8f4981598c16c5d2db7295b832321413e0c
User <User@DESKTOP-6JPNDEN.lan>
2024-01-04 10:56:03 -0500

    Learning

--- src/C/C.java
+++ src/C/C.java
@@ -4,7 +4,7 @@ public class C {
 	public static void main(String[] args) {
 		
 		System.out.println("Third time to let it sink in.");
-		System.out.println("B");
+		System.out.println("C");
 		System.out.println("");
 	}
 
